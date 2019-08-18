394fba3e22dba285fec1f17d945803be94bd3d93
Cris Silva Jr <ishiikurisu@yahoo.com>
2019-08-18 20:22:56 -0300

    naive implementation improvement

--- app.rb
+++ app.rb
@@ -7,16 +7,26 @@ end
 
 
 def combine xs
-  1.upto(xs.size).flat_map do |n|
-    xs.combination(n).to_a
-  end
+    1.upto(xs.size).flat_map do |n|
+        xs.combination(n).to_a
+    end
+end
+
+
+def price p
+    p.map { |e| e["p"] }.inject(0, :+)
+end
+
+
+def score p
+    p.map { |e| e["s"] }.inject(0, :+)
 end
 
 
 def validate m, p
     is_q = m["Q"].map { |k, v| v == p.filter { |e| e["c"] == k }.length }.all?
     is_t = p.length == m["T"]
-    is_w = p.map { |e| e["p"] }.inject(0, :+) < m["W"]
+    is_w = price(p) < m["W"]
     is_q && is_t && is_w
 end
 
@@ -25,8 +35,8 @@ def analyze fr_r, m
     combine(fr_r).filter { |p|
         validate(m, p)
     }.sort { |a, b|
-        b.map { |e| e["s"] }.inject(0, :+) <=> a.map { |e| e["s"] }.inject(0, :+)
-    }[0]
+        score(b) <=> score(a)
+    }.first
 end
 
 
