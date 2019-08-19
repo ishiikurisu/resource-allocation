fe45f69d23031234f974abfde4a131d07717b3f5
Cris Silva Jr <ishiikurisu@yahoo.com>
2019-08-18 21:04:31 -0300

    first naive implementation fail with big sample

---
+++ samples/big_case/generate.rb
@@ -0,0 +1,36 @@
+require "json"
+
+
+def new_player category
+    return {
+        "c" => category,
+        "s" => Random.rand(30) - 15,
+        "p" => Random.rand(28) + 2
+    }
+end
+
+
+def generate_data
+    outlet = []
+
+    20.times do
+        {
+            "G" => 2,
+            "Z" => 6,
+            "M" => 6,
+            "A" => 6,
+            "T" => 1,
+        }.each do |category, how_many|
+            how_many.times do
+                outlet << new_player(category)
+            end
+        end
+    end
+
+    return outlet
+end
+
+
+File.open("#{ARGV[0]}/fr_R.json", "w") do |fp|
+    fp.write generate_data.to_json
+end
